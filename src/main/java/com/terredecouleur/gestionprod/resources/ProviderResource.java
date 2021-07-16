872378cdd5e0a66f00ec8e203d8e8e195465ac28
MIQEAS MENDES <miqeasmendes@gmail.com>
2021-07-16 20:19:07 +0200

    First class Provider tested

--- src/main/java/com/terredecouleur/gestionprod/resources/ProviderResource.java
+++ src/main/java/com/terredecouleur/gestionprod/resources/ProviderResource.java
@@ -1,16 +1,28 @@
 package com.terredecouleur.gestionprod.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.terredecouleur.gestionprod.models.Provider;
 
 @RestController
 @RequestMapping(value="/providers")
 public class ProviderResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
+	public List<Provider> list() {
 		
-		return "REST is OK!";
+		Provider p1 = new Provider(1, "Cosmos", "7 rue Louis Niqueux", "37000");
+		Provider p2 = new Provider(2, "Pettra", "102 rue Louis Pasteur", "45000");
+		
+		List<Provider> list = new ArrayList<>();
+		list.add(p1);
+		list.add(p2);
+		
+		return list;
 	}
 }
