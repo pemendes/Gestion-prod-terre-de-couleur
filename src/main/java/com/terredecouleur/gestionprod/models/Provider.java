e4c364f43a33a6d2595fadd06085ad744ac91a3b
MIQEAS MENDES <miqeasmendes@gmail.com>
2021-07-17 01:03:09 +0200

    Add H2 db and database created

--- src/main/java/com/terredecouleur/gestionprod/models/Provider.java
+++ src/main/java/com/terredecouleur/gestionprod/models/Provider.java
@@ -2,10 +2,19 @@ package com.terredecouleur.gestionprod.models;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Provider implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String name;
 	private String address;
 	private String cp;
