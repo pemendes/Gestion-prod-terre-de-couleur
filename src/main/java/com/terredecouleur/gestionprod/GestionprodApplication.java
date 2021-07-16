42e0b0999e03e78f818833403180389b8fe6cc95
MIQEAS MENDES <miqeasmendes@gmail.com>
2021-07-16 19:28:48 +0200

    Project created

---
+++ src/main/java/com/terredecouleur/gestionprod/GestionprodApplication.java
@@ -0,0 +1,13 @@
+package com.terredecouleur.gestionprod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionprodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionprodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/terredecouleur/gestionprod/GestionprodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terredecouleur.gestionprod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionprodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
