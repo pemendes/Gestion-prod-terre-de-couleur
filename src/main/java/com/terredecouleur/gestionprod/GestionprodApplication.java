ee9e2802a47799046972e181c5cb613ba64f0cde
MIQEAS MENDES <miqeasmendes@gmail.com>
2021-07-18 11:33:08 +0200

    Save object operation created manualy

--- src/main/java/com/terredecouleur/gestionprod/GestionprodApplication.java
+++ src/main/java/com/terredecouleur/gestionprod/GestionprodApplication.java
@@ -1,13 +1,33 @@
 package com.terredecouleur.gestionprod;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.terredecouleur.gestionprod.models.Provider;
+import com.terredecouleur.gestionprod.repositories.ProviderRepository;
+
 @SpringBootApplication
-public class GestionprodApplication {
+public class GestionprodApplication implements CommandLineRunner {
+	
+	@Autowired
+	private ProviderRepository repo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionprodApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Provider p1 = new Provider(null, "Cosmos", "7 Rue Louis Niqueux", "37000");
+		Provider p2 = new Provider(null, "Terramus", "102 Rue Pasteur", "45000");
+		Provider p3 = new Provider(null, "Assemblor", "25 Rue Daniel Mayer", "37100");
+		
+		repo.saveAll(Arrays.asList(p1, p2, p3));
+		
 	}
 
 }
