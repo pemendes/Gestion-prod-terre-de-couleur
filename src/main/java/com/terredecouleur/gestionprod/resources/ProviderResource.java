aa1d8ea75ab50e5911e46601cb236d91b2e680c5
MIQEAS MENDES <miqeasmendes@gmail.com>
2021-07-26 12:56:44 +0200

    Paginate providers

--- src/main/java/com/terredecouleur/gestionprod/resources/ProviderResource.java
+++ src/main/java/com/terredecouleur/gestionprod/resources/ProviderResource.java
@@ -4,11 +4,13 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -54,5 +56,16 @@ public class ProviderResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<Provider>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		Page<Provider> list = service.findPage(page, linesPerPage, orderBy, direction);
+		  
+		return ResponseEntity.ok().body(list);
 	}
 }
